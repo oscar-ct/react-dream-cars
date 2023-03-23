a8ded074cd56d83bb7614cb26af9598f8595cddb
Oscar Castro <oscar.a.castro818@gmail.com>
2023-03-23 17:53:17 -0500

    feat: installed firebase and config file

---
+++ src/firebase.config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCxjfqboIG-SDh4EdaRC3lbR8MbbnKpPsI",
+    authDomain: "exotic-vehicle-marketplace-app.firebaseapp.com",
+    projectId: "exotic-vehicle-marketplace-app",
+    storageBucket: "exotic-vehicle-marketplace-app.appspot.com",
+    messagingSenderId: "589724064517",
+    appId: "1:589724064517:web:ad26f752d7da55a4fb756c"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+export const db = getFirestore();
