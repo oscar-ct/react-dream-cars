160b6a7eeedfe0b45a5ffec17d18fba836adf8eb
Oscar Castro <oscar.a.castro818@gmail.com>
2023-03-23 20:49:22 -0500

    feat: created route layout for pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import {BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Explore from "./pages/Explore";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignOut from "./pages/SignOut";
+
 
 function App() {
   return (
     <>
-      <h1>New App</h1>
+        <Router>
+            <Routes>
+                <Route path={"/"} element={<Explore/>}/>
+                <Route path={"/offers"} element={<ForgotPassword/>}/>
+                <Route path={"/sign-in"} element={<Offers/>}/>
+                <Route path={"/profile"} element={<Profile/>}/>
+                <Route path={"/sign-in"} element={<SignIn/>}/>
+                <Route path={"/sign-out"} element={<SignOut/>}/>
 
+            </Routes>
+        </Router>
     </>
   );
 }
