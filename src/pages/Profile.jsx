b0c5b5d3f4334e99f1dc4f41bfac9c326168971f
Oscar Castro <oscar.a.castro818@gmail.com>
2023-03-26 20:28:38 -0500

    feat: displayed username when signed in

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {getAuth} from "firebase/auth";
+
 
 const Profile = () => {
-    return (
-        <div>
-            Profile
-        </div>
-    );
+
+    const [user, setUser] = useState(null);
+
+    const auth = getAuth();
+    useEffect(function () {
+        setUser(auth.currentUser)
+    }, []);
+
+    if (user) {
+        return <h1>{user.displayName}</h1>
+    } else {
+        return (
+            <h1>Not logged In</h1>
+        );
+    }
+
 };
 
 export default Profile;
