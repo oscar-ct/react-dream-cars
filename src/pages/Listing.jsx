8512b4278569d1438005d56f1f4362a888ab881b
Oscar Castro <oscar.a.castro818@gmail.com>
2023-04-03 13:19:01 -0500

    feat: displayed listing data to DOM

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -15,8 +15,6 @@ const Listing = () => {
     const params = useParams();
     const auth = getAuth();
 
-
-
     useEffect(function() {
         const fetchListing = async () => {
             const docRef = doc(db, "listings", params.listingId);
@@ -26,17 +24,79 @@ const Listing = () => {
                 setListing(docSnap.data());
                 setLoading(false);
             }
+            console.log(listing)
         }
         fetchListing();
     }, [params.listingId]);
 
 
+    if (loading) {
+        return <h1>Loading...</h1>
 
-    return (
-        <div>
-            Listing
-        </div>
-    );
+    } else {
+
+        const { userRef, name, offer, discountedPrice, regularPrice, location, type, year, make, model, mileage} = listing;
+
+            return (
+                <main>
+                    Listing
+                    <div onClick={() => {
+                        navigator.clipboard.writeText(window.location.href);
+                        setShareLinkCopied(true);
+                        setTimeout(function () {
+                            setShareLinkCopied(false)
+                        }, 2000)
+                    }}>
+                        <img src={shareIcon} alt={"share"}/>
+                    </div>
+
+                    {shareLinkCopied && (
+                        <p>Link Copied!</p>
+                    )}
+
+                    <div>
+                        <p>
+                            {name} - ${offer ? discountedPrice : regularPrice}
+                        </p>
+                        <p>
+                            {location}
+                        </p>
+                        <p>
+                            For {type === "rent" ? "Rent" : "Sale"}
+                        </p>
+                        {offer && (
+                            <p>
+                                ${regularPrice - discountedPrice} discount
+                            </p>
+                        )}
+                        <ul>
+                            <li>
+                                {year}
+                            </li>
+                            <li>
+                                {make}
+                            </li>
+                            <li>
+                                {model}
+                            </li>
+                            <li>
+                                {mileage}
+                            </li>
+                        </ul>
+                        <p>
+                            Location
+                        </p>
+                        {/*MAP*/}
+                        {auth.currentUser?.uid !== userRef && (
+                            <Link to={`/contact/${userRef}?listingName=${name}&listingLocation=${location}`}>
+                                Contact Owner
+                            </Link>
+                        )}
+                    </div>
+
+                </main>
+        );
+    }
 };
 
 export default Listing;
