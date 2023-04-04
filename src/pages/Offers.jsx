f47aa44c4efb3ed58a63ecdf48b1cd8a7de8bcb9
Oscar Castro <oscar.a.castro818@gmail.com>
2023-04-04 11:42:45 -0500

    feat: added pagination to listings with view more button

--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -8,14 +8,21 @@ const Offers = () => {
 
     const [listings, setListings] = useState(null);
     const [loading, setLoading] = useState(true);
+    const [lastFetchedListing, setLastFetchedListing] = useState(null);
 
 
     useEffect(function() {
         const fetchListings = async () => {
             try {
                 const listingsRef = collection(db, "listings");
-                const q = query(listingsRef, where("offer", "==", true), orderBy("timestamp", "desc"), limit(10));
+                const q = query(
+                    listingsRef,
+                    where("offer", "==", true),
+                    orderBy("timestamp", "desc"),
+                    limit(1));
                 const querySnap = await getDocs(q);
+                const lastVisible = querySnap.docs[querySnap.docs.length - 1];
+                setLastFetchedListing(lastVisible);
                 let listings = [];
                 querySnap.forEach(function (doc) {
                     return listings.push({
@@ -31,6 +38,33 @@ const Offers = () => {
         }
         fetchListings();
     }, []);
+
+    const fetchMoreListings = async () => {
+        try {
+            const listingsRef = collection(db, "listings");
+            const q = query(
+                listingsRef,
+                where("offer", "==", true),
+                orderBy("timestamp", "desc"),
+                startAfter(lastFetchedListing),
+                limit(10));
+            const querySnap = await getDocs(q);
+            const lastVisible = querySnap.docs[querySnap.docs.length - 1];
+            setLastFetchedListing(lastVisible);
+            let listings = [];
+            querySnap.forEach(function (doc) {
+                return listings.push({
+                    id: doc.id,
+                    data: doc.data(),
+                })
+            });
+            setListings((prevState) => [...prevState, ...listings]);
+            setLoading(false);
+        } catch (e) {
+            toast.error("Could not fetch listings");
+        }
+    }
+
     return (
         <div className={"category"}>
             <header className={"page-header"}>
@@ -50,6 +84,12 @@ const Offers = () => {
                                 )}
                             </ul>
                         </main>
+                        <br/>
+                        {
+                            lastFetchedListing ? (
+                                <p className={"load-more"} onClick={fetchMoreListings}>Load More</p>
+                            ) : lastFetchedListing === undefined ? <p className={"no-listings"}>No more listings</p> : ""
+                        }
                     </>
                     :
                     <p>
