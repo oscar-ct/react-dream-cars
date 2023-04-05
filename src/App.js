3d7e4e46dbf9fc464a3895be6bc76d5853c1e217
Oscar Castro <oscar.a.castro818@gmail.com>
2023-04-04 21:06:13 -0500

    feat: created new route for edit listing

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import Category from "./pages/Category";
 import CreateListing from "./pages/CreateListing";
 import Listing from "./pages/Listing";
 import Contact from "./pages/Contact";
+import EditListing from "./pages/EditListing";
 
 
 function App() {
@@ -30,6 +31,7 @@ function App() {
                 <Route path={"/sign-in"} element={<SignIn/>}/>
                 <Route path={"/sign-up"} element={<SignUp/>}/>
                 <Route path={"/create-listing"} element={<CreateListing/>}/>
+                <Route path={"/edit-listing/:listingId"} element={<EditListing/>}/>
                 <Route path={"/category/:categoryName/:listingId"} element={<Listing/>}/>
                 <Route path={"/contact/:ownerId"} element={<Contact/>}/>
             </Routes>
