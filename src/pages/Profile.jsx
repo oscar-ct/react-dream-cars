db474fed93856a942d35446d9646125163a83568
Oscar Castro <oscar.a.castro818@gmail.com>
2023-04-06 02:10:04 -0500

    style: add white border box, minor updates

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -99,7 +99,7 @@ const Profile = () => {
 
     return (
 
-        <div className={"mt-4"}>
+        <div className={"mt-4 lg:mt-12"}>
             {/*<div className={"profile"}>*/}
             {/*    <header className={"profile-header"}>*/}
             {/*        <p className={"page-header"}>My Profile</p>*/}
@@ -160,12 +160,12 @@ const Profile = () => {
                 <main className={"flex flex-col"}>
                     <div>
                         <div className={"flex justify-center"}>
-                            <div className="card w-11/12 md:w-9/12 lg:w-1/2 bg-neutral text-neutral-content">
+                            <div className="card shadow-lg w-11/12 md:w-9/12 lg:w-1/2 bg-green-300/20 text-neutral">
                                 <div  className={"flex justify-between"}>
                                     <p className={"w-full pl-8 mt-3 text-center text-lg font-bold"}>Personal Details</p>
                                     <div>
                                     {!changeDetails ?
-                                        <EditIcon style={{cursor: "pointer"}} className={"mt-3 mr-3 mb-1"} fill={"white"} onClick={() => {onClick()}}/>
+                                        <EditIcon style={{cursor: "pointer"}} className={"mt-3 mr-3 mb-1"} fill={"neutral"} onClick={() => {onClick()}}/>
                                         :
                                         <button className="btn btn-primary btn-xs mt-3 mr-3 mb-1" onClick={() => {onClick()}}>done</button>
                                     }
@@ -184,6 +184,7 @@ const Profile = () => {
                                             onChange={onChangePersonalDetails}
                                         />
                                     </div>
+                                    <div className={"border-white"}/>
                                     <div className={"flex"}>
                                         <label className={"pt-3 pr-1"}>Email:</label>
                                         <input
@@ -205,12 +206,12 @@ const Profile = () => {
                         {!loading && listingsState?.length > 0 ? (
                             <>
                                 <div className={"mt-5"}>
-                                    <p className={"text-2xl font-bold"}>My Listings</p>
+                                    <p className={"text-lg font-bold"}>My Listings</p>
                                 </div>
                                 <div className={"flex flex-col lg:flex-row lg:justify-center w-full items-center"}>
 
                                     <div className={"flex justify-center flex-wrap"}>
-                                        <Link to={"/create-listing"} className="grid w-11/12 md:w-9/12 lg:w-96 h-20 lg:h-auto card bg-base-300 rounded-box place-items-center m-2 shadow-xl">
+                                        <Link to={"/create-listing"} className="grid w-11/12 md:w-9/12 lg:w-96 h-20 lg:h-auto card bg-base-300 rounded-box place-items-center m-2 shadow-lg">
                                             <img className={"h-8 lg:h-14"} src={plusIcon} alt={"plus"}/>
                                         </Link>
                                     {listingsState.map(function (listing) {
@@ -219,7 +220,19 @@ const Profile = () => {
                                     </div>
                                 </div>
                             </>
-                        ) : <p>You have no listings yet</p>}
+                        ) : <>
+                            <div className={"mt-5"}>
+                                <p className={"text-lg font-bold"}>No listings</p>
+                            </div>
+                            <div className={"flex flex-col lg:flex-row lg:justify-center w-full items-center"}>
+                                <div className={"flex justify-center flex-wrap"}>
+                                    <Link to={"/create-listing"} className="grid w-96 h-96  card bg-base-300 rounded-box place-items-center m-2 shadow-lg">
+                                    <img className={"h-8 lg:h-14"} src={plusIcon} alt={"plus"}/>
+                                    </Link>
+                                </div>
+                            </div>
+                        </>
+                        }
                     </div>
 
                 </main>
