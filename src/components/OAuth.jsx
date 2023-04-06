3f323e9895573561070cfec925ec71e76874e585
Oscar Castro <oscar.a.castro818@gmail.com>
2023-04-06 02:10:27 -0500

    text update

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -38,7 +38,7 @@ const OAuth = () => {
 
     return (
         <div className={"social-login"}>
-            <p>Sign {location.pathname === "/sign-up" ? "up" : "in"} with
+            <p>Or sign {location.pathname === "/sign-up" ? "up" : "in"} with
             </p>
             <button className={"social-icon-div"} onClick={onGoogleClick}>
                 <img className={"social-icon-img"} src={googleIcon} alt={"google"}/>
