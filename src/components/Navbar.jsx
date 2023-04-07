4b3ea97a82a663cfb3ac69f6e146a415a76f53f2
Oscar Castro <oscar.a.castro818@gmail.com>
2023-04-06 22:40:56 -0500

    feat: fixed navbar to top when scrolling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -74,7 +74,8 @@ const Navbar = () => {
         {/*    </nav>*/}
         {/*</footer>*/}
 
-        <div className="navbar bg-base-100 shadow-md">
+        {/*<div className="navbar bg-base-100 shadow-md">*/}
+        <div className={"navbar sticky top-0 z-40 w-full backdrop-blur flex-none transition-colors duration-500 lg:z-50 lg:border-b lg:border-slate-900/10 dark:border-slate-50/[0.06] bg-white/95 supports-backdrop-blur:bg-white/60 dark:bg-transparent"}>
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
