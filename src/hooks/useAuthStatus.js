87d52409fdac570ec08bc3de9cacfc2f5fc27c0b
Oscar Castro <oscar.a.castro818@gmail.com>
2023-04-07 23:38:48 -0500

    reset to original hook status

--- src/hooks/useAuthStatus.js
+++ src/hooks/useAuthStatus.js
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react';
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
+
 export const useAuthStatus = () => {
     const [loggedIn, setLoggedIn] = useState(false);
     const [loading, setLoading] = useState(true);
+
 
     useEffect(function () {
         const auth = getAuth();
@@ -13,6 +15,7 @@ export const useAuthStatus = () => {
            }
            setLoading(false);
        });
+       console.log("Checking auth status from useAuthStatus custom hook")
     });
 
 
