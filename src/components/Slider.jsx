35e2513cc48919deafa957775dd1da5a2af4c9b6
Oscar Castro <oscar.a.castro818@gmail.com>
2023-04-11 17:07:47 -0500

    feat: big update, filtered arrlist on type, added second slider, setup nav buttons

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { useNavigate } from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import { collection, getDocs, query, orderBy, limit } from "firebase/firestore";
 import { db } from "../firebase.config";
 import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from "swiper";
@@ -11,7 +11,8 @@ SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 const Slider = () => {
 
     const [loading, setLoading] = useState(true);
-    const [listingsState, setListingsState] = useState(null);
+    const [rentalListings, setRentalListings] = useState(null);
+    const [saleListings, setSaleListings] = useState(null);
 
     const navigate = useNavigate();
 
@@ -20,16 +21,27 @@ const Slider = () => {
             const listingsRef = collection(db, "listings");
             const q = query(listingsRef, orderBy("timestamp", "desc"), limit(5));
             const querySnap = await getDocs(q);
-            console.log(querySnap)
-            let listings = [];
+
+            let rListings = [];
+            let sListings = [];
 
             querySnap.forEach(function(doc) {
-               return  listings.push({
-                  id: doc.id,
-                  data: doc.data(),
-               });
+
+                if (doc.data().type === "rent") {
+                    rListings.push({
+                        id: doc.id,
+                        data: doc.data(),
+                    });
+                } else {
+                    sListings.push({
+                        id: doc.id,
+                        data: doc.data(),
+                    });
+                }
+
             });
-            setListingsState(listings);
+            setSaleListings(sListings);
+            setRentalListings(rListings);
             setLoading(false);
         }
         getListings();
@@ -40,29 +52,82 @@ const Slider = () => {
         return <h1>Loading...</h1>
     }
 
-    if (listingsState.length === 0) {
+    if (rentalListings.length === 0) {
         return <></>
     }
 
-    return listingsState && (
-        <>
-            <p className={"explore-heading"}>Recommended</p>
-            <Swiper slidesPerView={1} pagination={{clickable: true}}>
-                {listingsState.map(function ({data, id}) {
-                    return <SwiperSlide key={id} onClick={() => navigate(`/category/${data.type}/${id}`)}>
-                        <div className={"swiper-slide-div"}>
-                            <img src={data.imageUrls[0]} alt={"vehicles"} className={"swipe-img"}/>
-                            <p className={"swiper-slide-text"}>{data.name}</p>
-                            <p className={"swiper-slide-price"}>
-                                ${data.discountedPrice ?? data.regularPrice}
-                                {data.type === "rent" && "/ Day"}
-                            </p>
-                        </div>
+    return rentalListings && (
+        <div className={"w-full flex flex-col lg:flex-row"}>
 
-                    </SwiperSlide>
-                })}
-            </Swiper>
-        </>
+            <div className={"w-full lg:w-6/12"}>
+                <div className={"flex justify-center"}>
+                    <Link to={"/category/rent"} className={"btn btn-primary px-24 my-2"}>
+                        Rent
+                    </Link>
+                </div>
+                {/*<div className={"link flex py-1 justify-center bg-purple-400 hover:bg-neutral-focus/50"}>*/}
+                {/*    <Link to={"/category/rent"} className={"text-white link"}>Click here to view all our rentals</Link>*/}
+                {/*</div>*/}
+                <Swiper slidesPerView={1} pagination={{clickable: true}} navigation>
+                    {rentalListings.map(function ({data, id}) {
+                        return <SwiperSlide key={id}>
+                            {/*<div className={"swiper-slide-div"}>*/}
+                                {/*<div className={"w-full"}>*/}
+                                    {/*<button className="my-0 py-0 indicator-item indicator-middle indicator-center btn bg-purple-400/90 px-8">view all rentals</button>*/}
+                                    <img src={data.imageUrls[0]} alt={"vehicles"} className={"lg:h-96 swipe-img"}/>
+                                {/*</div>*/}
+                                <div className="indicator swiper-slide-text">
+                                    <span className="indicator-item badge badge-primary">${data.discountedPrice ?? data.regularPrice}{data.type === "rent" && "/Day"}</span>
+                                    <p className={""}>{data.name}</p>
+                                </div>
+
+                                <p onClick={() => navigate(`/category/${data.type}/${id}`)} className={"swiper-slide-text-link link"}>Click to view this listing</p>
+                            {/*</div>*/}
+
+                        </SwiperSlide>
+                    })}
+                </Swiper>
+
+            </div>
+            <div className={"w-full lg:w-6/12"}>
+                <div className={"flex justify-center"}>
+                    <Link to={"/category/sale"} className={"btn btn-primary px-24 my-2"}>
+                        Buy
+                    </Link>
+                </div>
+                {/*<div className={"link flex py-1 justify-center bg-green-300 hover:bg-neutral-focus/50"}>*/}
+                {/*    <Link to={"/category/sale"} className={"text-white link"}>Click here to view all our vehicles for sell</Link>*/}
+                {/*</div>*/}
+                <Swiper slidesPerView={1} pagination={{clickable: true}} navigation>
+                    {saleListings.map(function ({data, id}) {
+                        return <SwiperSlide key={id} >
+                            {/*<div className="indicator">*/}
+                            {/*    <span className="indicator-item indicator-middle indicator-center badge badge-secondary"/>*/}
+                            {/*    <div className="grid w-32 h-32 bg-base-300 place-items-center">content</div>*/}
+                            {/*</div>*/}
+
+                            {/*<div className={"swiper-slide-div"}>*/}
+
+
+                                {/*<div className={"w-full"}>*/}
+                                    {/*<button className="indicator-item indicator-middle indicator-center btn bg-green-400/90 px-8">rent</button>*/}
+                                    <img src={data.imageUrls[0]} alt={"vehicles"} className={"lg:h-96 swipe-img"}/>
+                                {/*</div>*/}
+                                <div  className="indicator swiper-slide-text">
+                                    <span className="indicator-item badge badge-primary">${data.discountedPrice ?? data.regularPrice}{data.type === "rent" && "/Day"}</span>
+                                    <p className={""}>{data.name}</p>
+                                </div>
+
+                                <p onClick={() => navigate(`/category/${data.type}/${id}`)} className={"swiper-slide-text-link link"}>Click to view this listing</p>
+
+                            {/*</div>*/}
+
+                        </SwiperSlide>
+
+                    })}
+                </Swiper>
+            </div>
+        </div>
     )
 };
 
