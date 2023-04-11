afe2b4cb6a7fd7ddb181db2ccf5bec0705b84106
Oscar Castro <oscar.a.castro818@gmail.com>
2023-04-11 17:02:14 -0500

    feat: created a list of fun facts

---
+++ src/context/Facts.js
@@ -0,0 +1,66 @@
+export const funFacts = () => {
+
+    return [
+        {
+            id: 1,
+            fact: "A mix between a Chihuahua and a dachshund is called a \"chiweenie.\""
+        },
+        {
+            id: 2,
+            fact: "There are no seagulls in Hawaii."
+        },
+        {
+            id: 3,
+            fact: "Even though dragonflies have six legs, they cannot walk."
+        },
+        {
+            id: 4,
+            fact: "Gummy bears were originally called \"dancing bears.\"["
+        },
+        {
+            id: 5,
+            fact: "Sea otters have the thickest fur of any mammal, at 1 million hairs per square inch."
+        },
+        {
+            id: 6,
+            fact: "The bird on the Twitter logo is named \"Larry.\" He was named after the basketball player Larry Bird, who played for the Boston Celtics."
+        },
+        {
+            id: 7,
+            fact: "The term \"coccyx\" (also known as your tailbone) is derived from the Greek word \"cuckoo\" (\"kokkux\") because the curved shape of the tailbone resembles the bird's beak."
+        },
+        {
+            id: 8,
+            fact: "A baby has around 30,000 taste buds. They are not just on the tongue but also on the sides, back, and roof of the mouth. Adults have about 10,000."
+        },
+        {
+            id: 9,
+            fact: "In one survey, three out of four people admitted to sharing an ice cream cone with their pet."
+        },
+        {
+            id: 10,
+            fact: "When humans take a breath, they replace only 15% of the air in their lungs with fresh air. When dolphins take a breath, they replace 90% of the air in their lungs with fresh air."
+        },
+        {
+            id: 11,
+            fact: "Feral pigs ate and completely destroyed $22,000 worth of cocaine that had been hidden in an Italian forest."
+        },
+        {
+            id: 12,
+            fact: "Pablo Picasso would often carry around a pistol loaded with blanks. He would fire it at people he found boring or anyone who insulted the Post-Impressionist painter, Paul Cézanne."
+        },
+        {
+            id: 13,
+            fact: "Monarch caterpillars breathe through holes in the sides of their bodies."
+        },
+        {
+            id: 14,
+            fact: "Male lobster’s bladders are in their heads, and when they fight, they squirt each other in the face with urine."
+        },
+        {
+            id: 15,
+            fact: "The word \"oysterhood\" means \"reclusiveness\" or \"an overwhelming desire to stay at home.\""
+        },
+    ]
+
+}
