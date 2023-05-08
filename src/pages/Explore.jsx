3b2e7282169ee43c1b07aaf82bb6a4c05dcec015
Oscar Castro <oscar.a.castro818@gmail.com>
2023-05-07 22:07:11 -0500

    feat: btn update

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -14,22 +14,21 @@ const Explore = () => {
                     {/*    <h1 className="text-3xl text-center text-blue-400">To get started, select a category to view.</h1>*/}
                     {/*</div>*/}
 
-                    <div className={"bg-black/80 lg:h-[32rem] flex flex-col"}>
+                    <div className={"bg-black/60 lg:h-[32rem] flex flex-col"}>
                         <div className={"flex px-4 lg:px-12 py-12 h-full flex-col items-center justify-center lg:items-start text-white w-full lg:w-8/12"}>
-                            <h1 className="text-4xl md:text-6xl">Making your dreams come true</h1>
-                            <p className={"mt-4 text-lg font-light lg:text-xl leading-8"}>We turn your dream car into a reality by providing you with the driving experience you will never forget. Choose from hundreds of luxurious, fast, prestigious vehicles.</p>
+                            <h1 className="text-4xl md:text-5xl font-bold">Making your dreams come true.</h1>
+                            <p className={"mt-4 text-lg font-light lg:text-xl leading-8"}>We turn your dream car into a reality by providing you with the opportunity to rent or purchase a driving experience you will never forget. Choose from hundreds of luxurious, fast, and affordable vehicles.</p>
                             <div className={"w-full flex justify-end"}>
                                 <div className={"w-4-/12 md:w-6/12 "}/>
                                 <div className={"w-8/12 md:w-6/12"}>
                                     <div className={"w-full flex justify-center mt-5 mb-3"}>
                                         <span className={"text-lg font-bold text-center"}>Ready to get behind the wheel?</span>
                                     </div>
-                                    <div className={"flex w-full justify-between"}>
-                                        <button onClick={() => navigate("/category/sale")} className={"btn px-10"}>buy</button>
-                                        <button onClick={() => navigate("/category/rent")} className={"btn px-10"}>rent</button>
+                                    <div className={"flex w-full justify-between "}>
+                                        <button onClick={() => navigate("/category/sale")} className={"btn btn-info px-10 text-xl"}>buy</button>
+                                        <button onClick={() => navigate("/category/rent")} className={"btn btn-info px-8 text-xl"}>rent</button>
                                     </div>
                                 </div>
-
                             </div>
                         </div>
                     </div>
